1d883c875928e9648bf8e850edbe30bc0607e3b5
Kir <Kirilla2012@gmail.com>
2025-04-14 00:17:16 +0300

    Функциональность «Рекомендации»

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.MpaRating;
-import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
