99b43ad4474935621bee9e49d10517a119185834
Mihey <miheyzhukov@yandex.ru>
2025-04-18 01:27:13 +0300

    исправление ошибок после тестов

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -202,6 +202,8 @@ public class FilmDbStorage implements FilmStorage {
     private Film mapRowToFilm(ResultSet rs, int rowNum) throws SQLException {
         int filmId = rs.getInt("film_id");
 
+        List<Director> directors = getDirectorForFilm(filmId);
+
         return Film.builder()
                 .id(filmId)
                 .name(rs.getString("name"))
@@ -213,6 +215,7 @@ public class FilmDbStorage implements FilmStorage {
                         rs.getString("mpa_name")))
                 .likes(getLikesForFilm(filmId))
                 .genres(getOrderedGenresForFilm(filmId)) // Упорядоченные жанры
+                .directors(directors != null ? directors : new ArrayList<>())
                 .build();
     }
 
@@ -303,15 +306,6 @@ public class FilmDbStorage implements FilmStorage {
                     "INSERT INTO film_directors (film_id, director_id) VALUES (?, ?)",
                     batchArgs);
         }
-    }
-
-    @Override
-    public Collection<Film> getDirectorFilms(Long directorId) {
-        String sql = "SELECT f.*, mr.name AS mpa_name FROM film_directors fd " +
-                "JOIN films f ON fd.film_id = f.film_id " +
-                "JOIN mpa_rating mr ON f.rating_id = mr.rating_id " +
-                "WHERE fd.director_id = ?";
-        return jdbcTemplate.query(sql, this::mapRowToFilm, directorId);
     }
 
     @Override
